041a480c0779be20e54c4e435a98af93d48d296c
vortecs <vortecs-one@gitlab.com>
2026-06-03 12:16:43 -0400

    three dots for top menu with options for profile, logout and settings

--- app/src/main/java/com/example/qhagoapp/MainActivity.kt
+++ app/src/main/java/com/example/qhagoapp/MainActivity.kt
@@ -74,14 +74,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Handle logout separately
         if (item.itemId == R.id.nav_logout) {
-            // Create an Intent to go back to LoginActivity
-            val intent = Intent(this, LoginActivity::class.java)
-            // Set flags to clear the activity stack, preventing users from going "back"
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            // Start the LoginActivity
-            startActivity(intent)
-            // Finish MainActivity to remove it from memory
-            finish()
+            logout()
             return true // Indicate the item was handled
         }
         // For all other items, let the Navigation Component handle it
@@ -92,29 +85,42 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
+    private fun logout() {
+        // Create an Intent to go back to LoginActivity
+        val intent = Intent(this, LoginActivity::class.java)
+        // Set flags to clear the activity stack, preventing users from going "back"
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        // Start the LoginActivity
+        startActivity(intent)
+        // Finish MainActivity to remove it from memory
+        finish()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean
     {
-        val result = super.onCreateOptionsMenu(menu)
-        // Using findViewById because NavigationView exists in different layout files
-        // between w600dp and w1240dp
-        val navView: NavigationView? = findViewById(R.id.nav_view)
-        if (navView == null) {
-            // The navigation drawer already has the items including the items in the overflow menu
-            // We only inflate the overflow menu if the navigation drawer isn't visible
-            menuInflater.inflate(R.menu.overflow, menu)
-        }
-        return result
+        menuInflater.inflate(R.menu.overflow, menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean
     {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Handle clicks on the overflow menu (e.g., top-right settings)
-        if (item.itemId == R.id.nav_settings) {
-            val navController = findNavController(R.id.nav_host_fragment_content_main)
-            navController.navigate(R.id.nav_settings)
-            return true
+        return when (item.itemId) {
+            R.id.nav_settings -> {
+                navController.navigate(R.id.nav_settings)
+                true
+            }
+            R.id.nav_slideshow -> {
+                navController.navigate(R.id.nav_slideshow)
+                true
+            }
+            R.id.nav_logout -> {
+                logout()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
-        return super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean
