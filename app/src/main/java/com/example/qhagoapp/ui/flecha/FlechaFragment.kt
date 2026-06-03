f5b8cc0a90934d01e1faa90800eb5c35a63126c1
vortecs <vortecs-one@gitlab.com>
2026-06-03 10:59:57 -0400

    UI/UX improvements

--- app/src/main/java/com/example/qhagoapp/ui/flecha/FlechaFragment.kt
+++ app/src/main/java/com/example/qhagoapp/ui/flecha/FlechaFragment.kt
@@ -1,4 +1,4 @@
-package com.example.qhagoapp.ui.webview
+package com.example.qhagoapp.ui.flecha
 
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
@@ -12,14 +12,14 @@ import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import com.example.qhagoapp.R
 
-class WebViewFragment : Fragment()
+class FlechaFragment : Fragment()
 {
     private lateinit var webView: WebView
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View
     {
-        val view = inflater.inflate(R.layout.fragment_webview, container, false)
+        val view = inflater.inflate(R.layout.fragment_flecha, container, false)
         webView = view.findViewById(R.id.webView)
         webView.webViewClient = object : WebViewClient()
         {
